eae7fb7603625c46483f8e665234792d18135160
minhmen93 <119550691+minhmen93@users.noreply.github.com>
2023-03-29 11:42:39 +0700

    Add files via upload

---
+++ src/arrayIndexOutOfBoundsException/ArrayExample.java
@@ -0,0 +1,16 @@
+package arrayIndexOutOfBoundsException;
+
+import java.util.Random;
+
+public class ArrayExample {
+    public Integer[] createRandom(){
+        Random rd = new Random();
+        Integer[] arr = new Integer[100];
+        System.out.println("Danh sach phan tu cua mang : ");
+        for (int i = 0; i < 100; i++) {
+            arr[i] = rd.nextInt(100);
+            System.out.print(arr[i]+" ");
+        }
+        return arr;
+    }
+}
---
+++ src/arrayIndexOutOfBoundsException/Main.java
@@ -0,0 +1,18 @@
+package arrayIndexOutOfBoundsException;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayExample arrayExample = new ArrayExample();
+        Integer[] arr = arrayExample.createRandom();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("\nVui lòng nhập chỉ số của 1 phần tử bất kỳ : ");
+        int x  = scanner.nextInt();
+        try{
+            System.out.println("Giá trị của phần tử có chỉ số "+x+" là "+arr[x]);
+        }catch (IndexOutOfBoundsException e){
+            System.err.println("Chỉ số vượt quá giới hạn của mảng ");
+        }
+    }
+}
